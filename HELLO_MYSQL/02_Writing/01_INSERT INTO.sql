5bb37786ffdee083fa7f45aa72606f4a679ebbe1
Borja <borja.mora.mendez@gmail.com>
2024-04-19 13:13:06 +0100

    first commit

---
+++ HELLO_MYSQL/02_Writing/01_INSERT INTO.sql
@@ -0,0 +1,11 @@
+-- Inserta un registro con identificadores, nombre y apellido en la tabla "users"
+
+INSERT INTO users (user_id, name, surname) VALUES (4, 'Jose', 'Lopez')
+
+-- Inserta un registro con nombre y apellido en la tabla "users"
+
+INSERT INTO users (name, surname) VALUES ('Jose', 'Lopez')
+
+-- Inserta un registro con identificador no correlativo, nombre y apellido en la tabla "users"
+
+INSERT INTO users (user_id, name, surname) VALUES (4, 'Jose', 'Lopez')
